8b317dff06be8444895903f2bb87b5145543fcee
Chirayu <tenguriachirayu@gmail.com>
2024-11-07 16:53:47 +0530

    Signup form done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
-import Login from './components/Login'
-import Register from './components/Register'
-import ForgetPassword from './components/ForgetPassword';
-
+import SignUp from './components/SignUp'
 
 function App() {
 
   return (
-   <Register/>
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<Login />} />
-    //     <Route path="/register" element={<Register />} />
-    //     <Route path="/reset-password" element={<ForgetPassword />} />
-    //   </Routes>
-    // </Router>
+
+    <SignUp />
+
   );
 }
 
