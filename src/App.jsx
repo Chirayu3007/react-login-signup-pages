8b96a4babde76097843355e27f29685ac6158fe7
Chirayu3007 <tenguriachirayu@gmail.com>
2024-11-09 13:54:08 +0530

    Addded Forget password pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
-import './App.css';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import Forget from "./components/Forget";
+import PhoneForgetPassword from "./components/PhoneForgetPassword";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
 const App = () => {
   const myRoutes = createBrowserRouter(
@@ -10,14 +17,22 @@ const App = () => {
       <Route>
         <Route path="/react-login-signup-pages" element={<Login />} />
         <Route path="/react-login-signup-pages/sign-up" element={<SignUp />} />
+        <Route
+          path="/react-login-signup-pages/forget-password"
+          element={<Forget />}
+        />
+        <Route
+          path="/react-login-signup-pages/forget-password-phone-number"
+          element={<PhoneForgetPassword />}
+        />
       </Route>
-  ))
+    )
+  );
 
-  return <RouterProvider router={myRoutes}/>
-}
+  return <RouterProvider router={myRoutes} />;
+};
 
 export default App;
-
 
 // const App = () => {
 //   const myRouter = createBrowserRouter([
